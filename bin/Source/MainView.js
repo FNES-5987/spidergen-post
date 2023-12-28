2176a60867e7862f3bde3e9d203706ba27424cc8
JJB <137266742+Jelly-Fish-J@users.noreply.github.com>
2023-12-28 18:00:59 +0900

    배열로 변환ing

--- bin/Source/MainView.js
+++ bin/Source/MainView.js
@@ -7,6 +7,8 @@ function MainView()
 {
 	AView.call(this);
 	
+	this.rowNum = 0;
+	this.postArray = [];
 }
 afc.extendsClass(MainView, AView);
 
@@ -20,6 +22,7 @@ MainView.prototype.init = function(context, evtListener)
 MainView.prototype.onInitDone = function()
 {
 	AView.prototype.onInitDone.call(this);
+	
 	
 
 };
@@ -47,7 +50,7 @@ MainView.prototype.table_row_click = function(comp, info, e)
 	
 	window.name = "parentForm";
 	
-	var index = this.grid_table.rowIndexOfCell(this.grid_table.getSelectedCells()[0]); // 선택된 row의 인덱스 값, 헤더 -1, 바디 0부터 시작
+	var index = this.grid_table.rowIndexOfCell(this.grid_table.getSelectedCells()[0]);
 
 	var selectRowData = this.grid_table.getRowData(index);
 	
@@ -74,78 +77,97 @@ MainView.prototype.table_row_click = function(comp, info, e)
 };
 
 MainView.prototype.onWindowResult = function(result, data, cntr) 
+{
+
+	var postNum = this.rowNum + 1;
+
+	if(result == 1)
 	{
-
-		var cntrId = cntr.getContainerId();
-		var rowNum = this.grid_table.getRowCount();
-		var postNum = rowNum + 1; // 게시글 번호는 1번부터 시작
-		
-		if(rowNum > 0) {
-			var iindex = this.grid_table.getRowSet(1);
-			console.log(iindex);
-		}
-
-		if(result == 1)
-		{
-			var postItem = {
-				name: data.name,
-				title: data.title,
-				content: data.content,
-				createTime: data.createTime,
-			};
-
-			var addRowItem = [postNum, data.title, data.name, data.createTime];
-			
-			var addRow = this.grid_table.prependRow(addRowItem, postItem);	
-			
-    	} else if(result == 2) {
-			var editItem = {
-				name: data.editName,
-				title: data.editTitle,
-				content: data.editContent,
-				createTime: data.editCreateTime,
-			};
-			
-			var num = rowNum - data.postNum;
-
-			var editRowItem = [data.postNum, data.editTitle, data.editName, data.editCreateTime];
-
-			var editRow = this.grid_table.setRow(num, editRowItem);
-			this.grid_table.setRowData(num, editItem);
-				
-		} else if(result == 3) {
-			console.log(rowNum - data);
-			this.grid_table.removeRow(rowNum - data);
-		}
-	};
-
-	MainView.prototype.click_btn_search = function(comp, info, e)
-	{
-		var select = this.search_select.getSelectedIndex();
-		console.log(select);
-		var inputText = this.search_input.getText();
-		console.log(inputText);
-		var rowNum = this.grid_table.getRowCount();
-		console.log(rowNum);
-
-		this.grid_table.createBackup(rowNum, rowNum);
-
-		var rowData = [];
-
-		for (i = 0; i < rowNum; i++) {
-			var row = this.grid_table.findRowByCellText(select, inputText);
-			console.log(row);
-
-			var cells = row.querySelectorAll('td');
-			cells.forEach(function(cell) {
-				rowData.push(cell.textContent.trim());
-				console.log(rowData);
-			});
-			
-			this.grid_table.addRow(rowData);
-			this.grid_table.removeRow(0);
-		}
-		this.grid_table.setDirectBackup(true);
-// 		for(; i < rowNum; i++) this.grid_table.addRow(rowData);
-	
+		var postItem = {
+			name: data.name,
+			title: data.title,
+			content: data.content,
+			createTime: data.createTime,
 		};
+
+		var addRowItem = [postNum, data.title, data.name, data.createTime, postItem];
+
+		var addRow = this.grid_table.prependRow(addRowItem, postItem);
+		this.rowNum++;
+		this.postArray.push(addRowItem);
+		console.log(this.postArray);
+
+	} else if(result == 2) {
+		var editItem = {
+			name: data.editName,
+			title: data.editTitle,
+			content: data.editContent,
+			createTime: data.editCreateTime,
+		};
+		
+		var num = this.rowNum - data.postNum;
+		
+		var editArr = this.postArray[num];
+		console.log(editArr);
+
+		var editRowItem = [data.postNum, data.editTitle, data.editName, data.editCreateTime, editItem];
+		
+		editArr = editRowItem;
+		
+		console.log(editArr);
+		console.log(this.postArray);
+
+		var editRow = this.grid_table.setRow(num, editRowItem);
+		this.grid_table.setRowData(num, editItem);
+
+	} else if(result == 3) {
+		this.grid_table.removeRow(this.rowNum - data);
+	}
+};
+
+
+MainView.prototype.click_btn_search = function(comp, info, e)
+{
+	
+	var select = this.search_select.getSelectedIndex();
+    var inputText = this.search_input.getText();
+    var rowNum = this.grid_table.getRowCount();
+    var colNum = this.grid_table.getColumnCount();
+
+    if (inputText === '') { 
+		return; 
+	} else {
+		var matchedRows = [];
+
+		for (var i = 0; i < rowNum; i++) {
+			var cellValue = this.grid_table.getCellText(i, select);
+
+			if (cellValue.includes(inputText)) {
+				var row = [];
+				for (var j = 0; j < colNum; j++) {
+					row.push(this.grid_table.getCellText(i, j));
+				}
+				row.push(this.grid_table.getRowData(i));
+				matchedRows.push(row);
+			}
+		}
+
+		this.grid_table.removeAll();
+
+		for (i = 0; i < matchedRows.length; i++) {
+			this.grid_table.addRow(matchedRows[i]);
+			var rowData = matchedRows[i][4];
+			this.grid_table.setRowData(0, rowData);
+		}
+	}
+
+   
+};
+MainView.prototype.press_enter = function(comp, info, e)
+{
+
+	if (e.originalEvent.key === 'Enter') {
+		this.click_btn_search();
+	}
+
+};
